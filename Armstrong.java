3c8d9608e2bc1b3e636d7d2754b89a9d29e62058
adee-31 <adee-31>
2025-06-27 04:42:03 +0530

    Add Armstrong number program

---
+++ Armstrong.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Armstrong
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        int r,sum=0;
+        int m=n;
+        
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum+(r*r*r);
+            n=n/10;
+        }
+        System.out.println(sum);
+        if (m==sum) {
+            System.out.println("Is a Armstrong number");
+            
+        }
+        else{
+            System.out.println("Not a Armsetrong numeber");
+        }
+    }
+}
