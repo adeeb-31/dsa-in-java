b378a01019afd32adc468ac3f2983c4d82cea871
adee-31 <adee-31>
2025-06-27 04:03:58 +0530

    Initial commit

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter a no.");
+        int n=s.nextInt();
+        int r,rev=0;
+        while(n>0){
+            r=n%10;
+            rev=rev*10+r;
+            n=n/10;
+            
+            
+        }
+        System.out.println(rev);
+      //  System.out.println("Try programiz.pro");
+    }
+}
