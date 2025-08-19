91c1bb7b413d3e455d628592c56d110484e20c2f
adee-31 <adee-31>
2025-08-19 19:33:53 +0530

    sum of array

---
+++ Sum_of_array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Sum_of_array {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter size of array");
+
+        int size=sc.nextInt();
+
+        int a[]= new int[size];
+
+        int sum=0;
+
+        System.out.println("Enter elements of array"+size);
+
+        for(int i=0;i<a.length;i++){
+
+        a[i]=sc.nextInt();
+
+            sum=sum+a[i];
+        }
+        System.out.println("Sum of arrray is:"+sum);
+    }
+}
+
